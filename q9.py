907fe09c9f7cc15018060ccd1b814caa081ef026
Vrund Patel <vrundpatel876@gmail.com>
2024-11-11 10:42:44 +0530

    Update q9.py

--- q9.py
+++ q9.py
@@ -1,10 +1,22 @@
-my_dict={
-    'apple':3,
-    'banana':5,
-    'mango':2,
-    'cherry':7,
-    'grapes':15
-}
-print("Dictionary item with their index:")
-for index,(key,value) in enumerate(my_dict.items()):
-    print(f"Index:{index},Key:{key},Value:{value}")
+n=int(input("Enter a number of dict:"))
+user_input=[]
+for i in range(n):
+    name=input("Enter a name:")
+    age=int(input("Enter a age:"))
+    roll=int(input("Enter a roll no:"))
+    course=input("Enter your course:")
+    div=input("Enter your division:")
+
+    my_dict={
+        'Name':name,
+        'Age':age,
+        'Roll':roll,
+        'Course':course,
+        'Div':div 
+    }
+    user_input.append(my_dict)
+
+for index,student in enumerate(user_input):
+    print(f"\nstudent:{index + 1}:")
+    for key,value in student.items():
+        print(f"Key: {key},Value: {value}")

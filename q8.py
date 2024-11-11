6fc32645342f6d7092b13d08dc19df34da768346
Vrund Patel <vrundpatel876@gmail.com>
2024-11-11 10:42:22 +0530

    Update q8.py

--- q8.py
+++ q8.py
@@ -1,13 +1,27 @@
-tuple1=(2,4,6,8,10)
+n=int(input("Enter a number:"))
+user_input=[]
+for i in range(n):
+    number=int(input(f"Enter number {i+1}:"))
+    user_input.append(number)
 
-print("using all operator in tupple")
-result=all(num % 2 == 0 for num in tuple1)
-print("The result is using all operator:",result)
+result=tuple(user_input)
+print(result)
 
-print("using any operator in tu")
-result1=any(num >= 5 for num in tuple1)
-print("The result is using any operator:",result1)
+print("1.All")
+print("2.any")
+print("3.enumerate")
 
-print("using enumerate in tupple")
-result=tuple(enumerate(tuple1))
-print("The result is using enumerate operator:",result)
+choice=int(input("Enter a your choice:"))
+
+if choice == 1:
+    print(all(result))
+
+elif choice == 2:
+    print(any(result))
+
+elif choice == 3:
+    enumerate_val=list(enumerate(result))
+    print(enumerate_val)
+
+else:
+    print("invalid choice")
